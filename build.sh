6d070fa8100c13cfa3d18210f5cdf26f7740c633
Digital Artifex <james.dudeck87@gmail.com>
2025-02-07 18:15:24 -0500

    Removed old scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if docker build -t qtcrossbuild .; then
-    if docker create --name tmpbuild qtcrossbuild; then
-        docker cp tmpbuild:/build/project/. ./output/
-        docker rm tmpbuild
-    fi
-fi
